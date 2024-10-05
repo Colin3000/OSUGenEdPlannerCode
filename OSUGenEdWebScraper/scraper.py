8173079e4f45e3493d308c9e2f15ebbc46c0ab89
Colin Lee <colinlee2024@gmail.com>
2024-10-05 00:30:49 -0400

    Finished web scraper and generated json of courses

--- OSUGenEdWebScraper/scraper.py
+++ OSUGenEdWebScraper/scraper.py
@@ -5,66 +5,101 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import json
+
+#Initalizes dictionary to later export as json
+genCourseData = {}
 chrome_options = Options() 
 chrome_options.add_experimental_option("detach", True)
 #Creates a webdriver to manipulate the starting page
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://courses.erppub.osu.edu/psc/ps/EMPLOYEE/PUB/c/COMMUNITY_ACCESS.OSR_CAT_SRCH.GBL?PortalActualURL=https%3a%2f%2fcourses.erppub.osu.edu%2fpsc%2fps%2fEMPLOYEE%2fPUB%2fc%2fCOMMUNITY_ACCESS.OSR_CAT_SRCH.GBL&PortalRegistryName=EMPLOYEE&PortalServletURI=https%3a%2f%2fcourses.erppub.osu.edu%2fpsp%2fps%2f&PortalURI=https%3a%2f%2fcourses.erppub.osu.edu%2fpsc%2fps%2f&PortalHostNode=CAMP&NoCrumbs=yes&PortalKeyStruct=yes")
+#Creates a webdriverwait to wait for certain elements to load
+wait = WebDriverWait(driver, 10)
+#Creates the list of courseAttributes needed
 courseAttributes = []
-for i in range(1, 8):
+for i in range(1, 9):
     courseAttributes.append("F" + str(i))
-#Finds the input form for the course attribute and course attribute value and input necessary data
+for i in range(1, 9):
+    courseAttributes.append("T" + str(i))
+#Finds the input form for the course attribute
 courseAttribute = driver.find_element(By.NAME, "OSR_CAT_SRCH_WK_CRSE_ATTR")
 courseAttribute.send_keys("GE2")
+#Getting the courses of each attribute
 for attribute in courseAttributes:
+    #Finds element for course attribute and enters data
     courseAttributeValue = driver.find_element(By.NAME, "OSR_CAT_SRCH_WK_CRSE_ATTR_VALUE")
     courseAttributeValue.clear()
     courseAttributeValue.send_keys(attribute)
     #Clicks the search button to load the list of courses
     driver.find_element(By.NAME, "OSR_CAT_SRCH_WK_BUTTON1").click()
-    wait = WebDriverWait(driver, 10)
     time.sleep(1)
+    #Creates a list to store info of courses
+    courseInfo = []
     #Check if the courses loaded has more than 1 page
     isPresent = driver.find_elements(By.ID, "OSR_CAT_SRCH_WK_BUTTON_FORWARD")
     #Don't look for a forward buttton if there is only one page
     if len(isPresent) == 0:
         #Waits until a course loads
         wait.until(EC.presence_of_element_located((By.ID, "OSR_CAT_SRCH_OSR_CRSE_HEADER$0")))
-        #Gets all the courses
+        #Gets course titles
         courseTitles = driver.find_elements(By.CLASS_NAME, "PSQRYTITLE")
+        #Converts course title elements to text
+        courseTitles = [element.text for element in courseTitles]
+        #Pops content header
         courseTitles.pop(0)
+        #Gets course descriptions
         courseDescriptions = driver.find_elements(By.CLASS_NAME, "PSLONGEDITBOX")
-        for title in courseTitles:
-            print(title.text)
-        for description in courseDescriptions:
-            print(description.text)
-        numCoursesOnPage = int(courseDescriptions[-1].get_attribute("id")[-2:])
-        for unitIndex in range(numCoursesOnPage):
+        #Gets the number of courses through course descriptions array
+        numCoursesOnPage = courseDescriptions[-1].get_attribute("id")[-2:]
+        if (not numCoursesOnPage.isnumeric()):
+            numCoursesOnPage = courseDescriptions[-1].get_attribute("id")[-1:]
+        numCoursesOnPage = int(numCoursesOnPage)
+        #Converts course description elements to text
+        courseDescriptions = [element.text for element in courseDescriptions]
+        courseUnits = []
+        #Finds the number of units in course
+        for unitIndex in range(1, numCoursesOnPage + 1):
             idToSearch = "OSR_CAT_SRCH_OSR_UNITS_DESCR$" + str(unitIndex)
-            print(driver.find_element(By.ID, idToSearch).text)
-
+            courseUnits.append(driver.find_element(By.ID, idToSearch).text)
+        #Adds course data to courseInfo
+        courseInfo = [{"Title": t, "Description": d, "Units": u} for t, d, u in zip(courseTitles, courseDescriptions, courseUnits)]
     else:
         wait.until(EC.presence_of_element_located((By.ID, "OSR_CAT_SRCH_WK_BUTTON_FORWARD")))
         onceMore = iter([True, False])
+        #Loop to go through pages as long as the forward button is enabled + one extra iteration
         while driver.find_element(By.ID, "OSR_CAT_SRCH_WK_BUTTON_FORWARD").is_enabled() or next(onceMore):
             wait.until(EC.presence_of_element_located((By.ID, "OSR_CAT_SRCH_OSR_CRSE_HEADER$0")))
             courseTitles = driver.find_elements(By.CLASS_NAME, "PSQRYTITLE")
+            courseTitles = [element.text for element in courseTitles]
             courseTitles.pop(0)
             courseDescriptions = driver.find_elements(By.CLASS_NAME, "PSLONGEDITBOX")
-            for title in courseTitles:
-                print(title.text)
-            for description in courseDescriptions:
-                print(description.text)
-            numCoursesOnPage = int(courseDescriptions[-1].get_attribute("id")[-2:])
-            for unitIndex in range(numCoursesOnPage):
+            numCoursesOnPage = courseDescriptions[-1].get_attribute("id")[-2:]
+            if (not numCoursesOnPage.isnumeric()):
+                numCoursesOnPage = courseDescriptions[-1].get_attribute("id")[-1:]
+            numCoursesOnPage = int(numCoursesOnPage)
+            courseDescriptions = [element.text for element in courseDescriptions]
+            courseUnits = []
+            for unitIndex in range(1, numCoursesOnPage + 1):
                 idToSearch = "OSR_CAT_SRCH_OSR_UNITS_DESCR$" + str(unitIndex)
-                print(driver.find_element(By.ID, idToSearch).text)
+                courseUnits.append(driver.find_element(By.ID, idToSearch).text)
+            #Adds course data of current page and adds to courseInfo
+            currentPageInfo = [{"Title": t, "Description": d, "Units": u} for t, d, u in zip(courseTitles, courseDescriptions, courseUnits)]
+            courseInfo = courseInfo + currentPageInfo
             #Finds and clicks the next page button
             driver.find_element(By.ID, "OSR_CAT_SRCH_WK_BUTTON_FORWARD").click()
             time.sleep(1)
             wait.until(EC.presence_of_element_located((By.ID, "OSR_CAT_SRCH_WK_BUTTON_FORWARD")))
+    #Adds courseInfo to genCourseData with attribute as key
+    genCourseData[attribute] = courseInfo
     #Go back to the course attribute page
     time.sleep(1)
     wait.until(EC.presence_of_element_located((By.ID, "OSR_CAT_SRCH_WK_HYPERLINK")))
     driver.find_element(By.ID, "OSR_CAT_SRCH_WK_HYPERLINK").click()
     time.sleep(1)
+#Closes webdriver
+driver.close()
+#Converts and writes getCourseData to json
+course_json = json.dumps(genCourseData, indent = 4)
+with open("GECourseData.json", "w") as outfile:
+    outfile.write(course_json)
